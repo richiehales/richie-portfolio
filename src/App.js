5987d83e7573ace16f7d452a48e82905bc938b90
Richie <richiecontrolsystems@outlook.com>
2023-10-12 17:07:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Header  from './features/Header/Header';
 
 function App() {
   return (
+    
     <div className="App">
+      <Header />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Step 1 - Create Nav Bar
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Richie Hales Portfolio
         </a>
       </header>
     </div>
---
+++ src/features/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Button from '@mui/material/Button';
+
+const Header = () => {
+
+
+  return (
+    <header>
+      <div className="logo">
+        <p>
+          Richie Hales Web Developer
+        </p>
+        <Button variant="contained">
+          Hello world
+        </Button>
+      </div>
+    </header>
+  );
+
+
+
+}
+
+
+
+export default Header;
