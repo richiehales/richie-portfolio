406dc36439e04c71c63b2e78f8c731db78ee38fa
Richie <richiecontrolsystems@outlook.com>
2023-10-16 14:30:10 +0100

    Completed V1.0

--- src/features/Contact/Contact.jsx
+++ src/features/Contact/Contact.jsx
@@ -1,20 +1,86 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Image from 'mui-image'
+import Link from '@mui/material/Link';
+import map from './images/map.JPG'
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import EmailIcon from '@mui/icons-material/Email';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+
+
+
+const imageStyle = {
+  maxWidth: '100%', // Ensure the image doesn't exceed its container width
+  height: 'auto', // Maintain the image's aspect ratio
+};
+
 
 
 export function Contact() {
 
   return (
     <Container>
-      <Typography                    // Net Ninja Tutorial 2
-        variant="h1"
+      <Box mb={4} />
+      <Typography                     
+        variant="h4"
         gutterBottom
         color="textSecondary"
-        align="center"   
-      >
-        Contact Component
+        align="center">
+          Contact
       </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={4} >
+        <Box mb={6} />
+        <List>
+            <ListItem button key="Email" component="a" href="mailto:richiecontrolsystems@outlook.com">
+              <ListItemIcon>
+                <EmailIcon />
+              </ListItemIcon>
+              <ListItemText>
+                <Link>Email</Link>
+              </ListItemText>      
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <LinkedInIcon />
+              </ListItemIcon>
+              <ListItemText>
+                <Link href="https://www.linkedin.com/in/richie-hales-472686131">LinkedIn</Link>
+              </ListItemText>      
+            </ListItem >
+            <ListItem button>
+              <ListItemIcon>
+                <FacebookIcon />
+              </ListItemIcon>
+              <ListItemText>
+                <Link href="https://www.facebook.com/profile.php?id=100084223683969">facebook</Link>
+              </ListItemText>      
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <GitHubIcon />
+              </ListItemIcon>
+              <ListItemText>
+                <Link href="https://github.com/richiehales">GitHub</Link>
+              </ListItemText>      
+            </ListItem>
+        </List> 
+        </Grid>
+        <Grid item xs={12} md={4} ></Grid>
+        <Grid item xs={12} md={4} >
+          <Image src={map} alt="Map" style={imageStyle}/>
+        </Grid>
+      </Grid>
+  
     </Container>
   )
 }
